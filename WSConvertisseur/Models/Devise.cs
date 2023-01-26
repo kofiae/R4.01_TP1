826a43e0cf556a0f39783e5c6261bf28e38234b6
odufejoo <olubusola.odufejoogoe@etu.univ-smb.fr>
2023-01-26 08:38:20 +0100

    Ajout du modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,61 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomDevise;
+        private double taux;
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            this.id = id;
+            this.nomDevise = nomDevise;
+            this.taux = taux;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string? NomDevise
+        {
+            get
+            {
+                return nomDevise;
+            }
+
+            set
+            {
+                nomDevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return taux;
+            }
+
+            set
+            {
+                taux = value;
+            }
+        }
+
+        
+    }
+}
